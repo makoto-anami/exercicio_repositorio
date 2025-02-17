54708f1f4309fdc8f5b961d346902205ba6e5a56
makoto-anami <anami.r.m@gmail.com>
2025-02-17 17:14:42 -0300

    commit #5

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Change #2");
         System.out.println("Change #3");
         System.out.println("Change #4");
+        System.out.println("Change #5");
     }
 }
